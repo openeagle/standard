d8c8d67a87936e4083f5986ae7abc713292d3c3e
zhbhun <zhbhun@gmail.com>
2022-06-10 17:24:31 +0800

    fix: 优化 commitlint 配置
    1. 去掉自定义的 type-enum
    2. 去掉 body 最大行长度限制

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -1,11 +1,6 @@
-// 相关配置 https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'refactor', 'docs', 'chore', 'style'],
-    ],
+    'body-max-line-length': [0, 'always', 1000],
   },
 }
